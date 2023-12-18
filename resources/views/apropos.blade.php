a4cd7119163a775270099637ec50d45e7c8a4953
Just Vallin--Detrez <just_vallin--detrez@ens.univ-artois.fr>
2023-12-18 15:15:45 +0100

    Création de la page A propos #54

---
+++ resources/views/apropos.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <h1>À propos de nous</h1>
+    <p>Ce site web est une démonstration des composants blade de Laravel. C'est un site simple qui démontre la puissance du moteur de template de Laravel.</p>
+</x-layout>
