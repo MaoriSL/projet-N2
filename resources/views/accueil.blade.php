455ce62b228428a7e4fa6a0b662d3616d410bad0
Just Vallin--Detrez <just_vallin--detrez@ens.univ-artois.fr>
2023-12-18 15:14:34 +0100

    Création de la page d'accueil #52

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -1,19 +1,3 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    @vite(['resources/scss/app.scss','resources/css/app.css','resources/js/app.js'])
-</head>
-<body>
-<div class="container py-4 px-3 mx-auto">
-    <h1>Hello, Bootstrap and Vite!</h1>
-    <button class="btn btn-primary">Primary button</button>
-</div>
-</body>
-</html>
+<x-layout>
+    <h1>Bienvenue sur la page d'accueil</h1>
+</x-layout>
