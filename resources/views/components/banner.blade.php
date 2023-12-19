216c40a02d6421e0a5ea69fae819297e822d2559
Ethan Copin <ethan_copin@ens.univ-artois.fr>
2023-12-19 09:20:52 +0100

    ajout de style

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,8 +1,15 @@
-<button class="btn btn-primary m-xl-2"><a class="text-white text-decoration-none" href='/'>🏛 Accueil</a></button>
-@auth
-    @if (Auth::user())
-        <button class="btn btn-primary"><a class="text-white text-decoration-none" href="{{route("liste")}}">📋 Scenes</a></button>
-    @endif
-@endauth
-<button class="btn btn-primary"><a class="text-white text-decoration-none" href="{{route("apropos")}}">💬 A propos</a></button>
-<button class="btn btn-primary"><a class="text-white text-decoration-none" href="{{route("contact")}}">📞 Contact</a></button>
+<nav class="navbar navbar-expand-lg navbar-light bg-dark text-white w-100">
+    <div class="collapse navbar-collapse">
+        <button class="btn btn-primary m-xl-2"><a class="text-white text-decoration-none" href='/'>🏛 Accueil</a></button>
+        @auth
+            @if (Auth::user())
+                <button class="btn btn-primary m-xl-2"><a class="text-white text-decoration-none" href="{{route("liste")}}">📋 Scenes</a></button>
+            @endif
+        @endauth
+        <button class="btn btn-primary m-xl-2"><a class="text-white text-decoration-none" href="{{route("apropos")}}">💬 A propos</a></button>
+        <button class="btn btn-primary m-xl-2"><a class="text-white text-decoration-none" href="{{route("contact")}}">📞 Contact</a></button>
+    </div>
+    <menu>
+        <x-header></x-header>
+    </menu>
+</nav>
